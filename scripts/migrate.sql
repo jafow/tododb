cc56bdbf01c71023579a97d5a304ed505be8d5d4
jafow <jared.a.fowler@gmail.com>
2020-02-04 18:59:23 -0800

    fix syntax

--- scripts/migrate.sql
+++ scripts/migrate.sql
@@ -15,7 +15,7 @@ insert into td.template_lists(title, description, items, owner_id) VALUES
     ('Night time', 'sleep routine', '{"items": [1]}', 2),
     ('Exam study', 'study routine', '{"items": [2]}', 2),
     ('open heart surgery', 'surgery', '{"items": [1, 2]}', 1)
-) returning td.template_lists.id, td.template_lists.title, td.template_lists.owner_id;
+returning td.template_lists.id, td.template_lists.title, td.template_lists.owner_id;
 
 
 --- active list
@@ -23,7 +23,7 @@ insert into td.active_lists(title, description, items, owner_id) VALUES
     ('Exam study', 'checklist for exam study topics', '{"items": [1, 4]}', 1),
     ('Night time routine', 'getting ready for sleep', '{"items": [7, 3, 5]}', 1),
     ('open heart surgery', 'things to do before cut into a persons chest', '{"items": [2, 6]}', 2)
-) returning td.active_lists.id, td.active_lists.title, td.active_lists.owner_id;
+returning td.active_lists.id, td.active_lists.title, td.active_lists.owner_id;
 
 
 --- list items
@@ -35,5 +35,4 @@ insert into td.list_items (description, parent_id) VALUES
     ('netflix', 2),
     ('do drugs', 3),
     ('do drugs', 2)
-)
 RETURNING td.list_items.id as item_id, td.list_items.description;
