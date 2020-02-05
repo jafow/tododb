5de9a38a1b730d9f06f49f273936e329bc0caab7
jafow <jared.a.fowler@gmail.com>
2020-02-04 18:54:15 -0800

    add tables and seed data

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-# db connection 
+# db connection
 DB_HOST?=localhost
 DB_PORT?=5436
 DB_USER?=todoadmin
 DB_NAME?=tododb
 # pro tip: override this with DB_PASSWORD="mysecurepassword" make
 DB_PASSWORD?=pizza
+DB_CONN=-U $(DB_USER) -d $(DB_NAME) -h $(DB_HOST) -p $(DB_PORT)
 
 # dirs
 HOME?=$(HOME)
@@ -12,19 +13,26 @@ PGPASS?=$(HOME)/.pgpass
 BUILDER=${CURDIR}/builder.sql
 SCRIPTS=${CURDIR}/scripts
 
+
 # targets
 all: migrate
-	psql -U $(DB_USER) -d $(DB_NAME) -h $(DB_HOST) -f $(BUILDER)
+	psql -w --quiet $(DB_CONN) -f $(BUILDER)
 
 migrate: init tables
 	# add additional targets as needed...
 	@cat $(SCRIPTS)/migrate.sql >> $(BUILDER)
 
+tables:
+	# build tables
+	@cat ${SCRIPTS}/tables.sql >> $(BUILDER)
+
 init: $(PGPASS)
+	# initalize
 	@psql -U postgres -h $(DB_HOST) -d postgres -p $(DB_PORT) --set=dbpass=$(DB_PASSWORD) -f $(SCRIPTS)/init.sql
 
 $(PGPASS):
 	@echo "$(DB_HOST):$(DB_PORT):$(DB_NAME):$(DB_USER):$(DB_PASSWORD)" >> "$(HOME)/.pgpass"
 
-tables:
-	@cat ${SCRIPTS}/tables.sql >> $(BUILDER)
+clean:
+	rm $(BUILDER)
+
--- scripts/tables.sql
+++ scripts/tables.sql
@@ -1,22 +1,67 @@
+--- set to one of DEBUG1,DEBUG2...DEBUG4,NOTICE for debugging
+set client_min_messages = warning;
+
 ---############################################################-
 --- schema
----############################################################ 
+---############################################################
 drop schema if exists td cascade;
 create schema td;
 
 
 ---############################################################-
 --- access privileges
----############################################################ 
+---############################################################
 ALTER DEFAULT PRIVILEGES IN SCHEMA td GRANT SELECT ON TABLES to PUBLIC;
 ALTER DEFAULT PRIVILEGES IN SCHEMA td GRANT INSERT,UPDATE ON TABLES to todouser;
 
 --- create account table
-drop table if exists accounts;
+drop table if exists td.accounts;
 
-create table accounts(
-    id bigserial serial primary key,
+create table td.accounts(
+    id serial primary key,
     email varchar(255) not null,
     created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
+--- create template_list table
+
+drop table if exists td.template_lists;
+
+create table td.template_lists(
+    id serial primary key,
+    title varchar(255) not null,
+    description varchar(255),
+    items json,
+    owner_id integer REFERENCES td.accounts(id),
+
+    expires_at timestamp default NULL,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
+
+--- active lists
+create table td.active_lists (
+    id serial primary key,
+    title varchar(255) not null,
+    description varchar(255),
+    items json,
+    owner_id integer REFERENCES td.accounts(id),
+
+    expires_at timestamp default NULL,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
+
+create table td.list_items (
+    id serial primary key,
+    description varchar(255),
+    complete boolean default FALSE,
+
+    parent_id integer REFERENCES td.active_lists(id),
+    expires_at timestamp default NULL,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
